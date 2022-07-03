9c45c76bbe08afcc93e0556101c8028b2cfe61de
Shivu-a <sebaingame@gmail.com>
2022-07-03 17:09:18 -0300

    12-operador-condicional-ternario

---
+++ 02-javascript-fundamentos-repaso/src/bases/12-operador-condicional-ternario.js
@@ -0,0 +1,15 @@
+import "./index.css";
+
+// Operador condicional ternario
+const activo = true;
+
+const mensaje = activo ? "Activo" : "Inactivo";
+
+console.log(mensaje);
+
+// sin necesidad del "else"
+const condicion = true;
+
+const unico = condicion && "Activo: unico";
+
+console.log(unico);
