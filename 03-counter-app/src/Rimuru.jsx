ac210d2599cb788380ec124d9c82a378bc305a8e
Shivu-a <sebaingame@gmail.com>
2022-07-04 23:29:26 -0300

    Rimuru file + learned how to pass multiple elements

---
+++ 03-counter-app/src/Rimuru.jsx
@@ -0,0 +1,10 @@
+const Rimuru = () => {
+  return (
+    <>
+      <h1>Rimuru</h1>
+      <h2>Tempest</h2>
+    </>
+  );
+};
+
+export default Rimuru;
--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
 import App from "./FirstApp";
+import Rimuru from "./Rimuru";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Rimuru></Rimuru>
   </React.StrictMode>
 );
