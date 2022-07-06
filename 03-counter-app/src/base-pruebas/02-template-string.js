e8cb32abc6099f1b0cf8a811e564c8d47977d5fc
Shivu-a <sebaingame@gmail.com>
2022-07-05 22:28:12 -0300

    02-template-string.test

--- 03-counter-app/src/base-pruebas/02-template-string.js
+++ 03-counter-app/src/base-pruebas/02-template-string.js
@@ -2,6 +2,8 @@ function getSaludo(nombre) {
   return "Hola " + nombre;
 }
 
-console.log(`Este es un texto: ${getSaludo(nombre)}  `);
+// console.log(`Este es un texto: ${getSaludo(nombre)}  `);
+
+// const nombre = "Pedro";
 
 export default getSaludo;
