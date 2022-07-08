0f1e3fa8e7e66fc33f848b2f56df584378eae4cc
Shivu-a <sebaingame@gmail.com>
2022-07-07 21:09:12 -0300

    React-Testing-Library

--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
-// import App from "./FirstApp";
+import App from "./FirstApp";
 import CounterApp from "./CounterApp";
 import RimuruFunction from "./Rimuru";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App title="Hola Mundo" /> */}
-    {/* <RimuruFunction></RimuruFunction> */}
-    <CounterApp value={7}></CounterApp>
+    <App title="Hola Mundo" />
+    <RimuruFunction></RimuruFunction>
+    {/* <CounterApp value={7}></CounterApp> */}
   </React.StrictMode>
 );
