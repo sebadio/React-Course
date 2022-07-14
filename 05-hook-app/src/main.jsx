8075080ab64e1dd0eed3229e004a05aa4850f53b
Shivu-a <sebaingame@gmail.com>
2022-07-13 23:26:27 -0300

    CounterApp.jsx

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { CounterApp } from "./01-useState/CounterApp";
 import { HooksAPP } from "./HooksAPP";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HooksAPP />
+    {/* <HooksAPP /> */}
+    <CounterApp />
   </React.StrictMode>
 );
