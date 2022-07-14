a099bef5485dd3443b73d8f67f682c4c7729c6c8
Shivu-a <sebaingame@gmail.com>
2022-07-13 22:56:22 -0300

    05-hooks-app first commit

---
+++ 05-hook-app/src/HooksApp.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HooksAPP = () => {
+  return <h1>HooksAPP</h1>;
+};
---
+++ 05-hook-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HooksAPP } from "./HooksAPP";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HooksAPP />
+  </React.StrictMode>
+);
