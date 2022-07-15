d956c6df8be8105381d6a62bb112bba88a8aac28
Shivu-a <sebaingame@gmail.com>
2022-07-14 22:36:15 -0300

    MultipleCustomHooks.jsx

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -6,13 +6,22 @@ export const MultipleCustomHooks = () => {
     "https://www.breakingbadapi.com/api/quotes/1"
   );
 
-  console.log(data, isLoading, error);
+  const { author, quote } = !!data && data[0];
 
   return (
     <>
       <h1>Breaking Bad Quoutes</h1>
 
       <hr />
+
+      {isLoading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-1">{quote}</p>
+          <footer className="blockquote-footer mt-1">{author}</footer>
+        </blockquote>
+      )}
     </>
   );
 };
